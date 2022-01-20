634dceed6a349243593072cf73a0a4d07dd7b7dd
Josh Mathiak <jmathiak4@gmail.com>
2022-01-20 02:18:58 -0600

    Added different views, added marking w/re-filtering, added re-filtering where necessary

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,6 @@
+import { isNgTemplate } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+
 
 interface toDoObject {
   toDo: string;
@@ -19,17 +21,107 @@ export class FormComponent implements OnInit {
   public todo = "";
   public obj = "";
   public toDoList = new Array();
+  public filteredList = new Array();
+  public displayStatus = "all";
+  public addValue = false;
 
+  public addValueList = [
+    {
+      display:"Incomplete",
+      value:false
+    },
+    {
+      display:"Complete",
+      value:true
+    }
+  ]
+  onChange(selectedValue:any)
+  {
+    console.log(selectedValue);
+    this.addValue=selectedValue;
 
+  }
   onClick(){
     let tdo = {
       toDo: this.todo,
-      complete: false
+      complete: this.addValue
     };
+    console.log(tdo)
     this.toDoList.push(tdo);
+    if(this.displayStatus === "Complete")
+    {
+      this.filteredList = this.toDoList.filter(items => items.complete===true)
+    }else if(this.displayStatus === "Incomplete")
+    {
+      this.filteredList = this.toDoList.filter(items => items.complete===false)
+    }else{
+      this.filteredList = this.toDoList
+    }
   }
 
+  showComplete(){
+    this.displayStatus = "Complete";
+    this.filteredList = this.toDoList.filter(items => items.complete==='true')
+  }
 
+  
+  showIncomplete(){
+    this.displayStatus = "Incomplete";
+    this.filteredList = this.toDoList.filter(items => items.complete===false)
+  }
+  showAll(){
+    this.displayStatus = "All";
+    this.filteredList = this.toDoList;
+  }
+
+  markIncomplete(ioi:any)
+  {
+    let index = this.toDoList.indexOf(ioi);
+    ioi.complete = false;
+    this.toDoList[index] = ioi;
+    if(this.displayStatus === "Complete")
+    {
+      this.filteredList = this.toDoList.filter(items => items.complete===true)
+    }else if(this.displayStatus === "Incomplete")
+    {
+      this.filteredList = this.toDoList.filter(items => items.complete===false)
+    }else{
+      this.filteredList = this.toDoList
+    }
+
+  }
+
+  markComplete(ioi:any)
+  {
+    let index = this.toDoList.indexOf(ioi);
+    ioi.complete = true;
+    this.toDoList[index] = ioi;
+    if(this.displayStatus === "Complete")
+    {
+      this.filteredList = this.toDoList.filter(items => items.complete===true)
+    }else if(this.displayStatus === "Incomplete")
+    {
+      this.filteredList = this.toDoList.filter(items => items.complete===false)
+    }else{
+      this.filteredList = this.toDoList
+    }
+
+  }
+
+  deleteItem(ioi:any)
+  {
+    let index = this.toDoList.indexOf(ioi);
+    this.toDoList.splice(index, 1);
+    if(this.displayStatus === "Complete")
+    {
+      this.filteredList = this.toDoList.filter(items => items.complete===true)
+    }else if(this.displayStatus === "Incomplete")
+    {
+      this.filteredList = this.toDoList.filter(items => items.complete===false)
+    }else{
+      this.filteredList = this.toDoList
+    }
+  }
   constructor() { }
 
   ngOnInit(): void {
